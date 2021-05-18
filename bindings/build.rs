2317987e57be3500a5ee2dbc2afa956a8f5deba2
Mohammad Danesh <mohammad@danesh.dev>
2021-05-18 19:55:27 +0430

    Implement create_window function for WinTray

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,9 +1,9 @@
 fn main() {
     windows::build!(
-        Windows::Win32::System::SystemServices::{LRESULT, PSTR},
+        Windows::Win32::System::SystemServices::{GetModuleHandleA, LRESULT, PSTR},
         Windows::Win32::UI::WindowsAndMessaging::{
             DefWindowProcA, HWND, LPARAM, RegisterClassA, WM_LBUTTONDOWN, WM_RBUTTONDOWN,
-            WNDCLASSA, WPARAM,
+            WNDCLASSA, WPARAM, WINDOW_STYLE, CreateWindowExA,
         },
     );
 }
--- src/wintray.rs
+++ src/wintray.rs
@@ -1,5 +1,7 @@
+use std::ptr;
+
 use bindings::Windows::Win32::{
-    System::SystemServices::{LRESULT, PSTR},
+    System::SystemServices::{self, HINSTANCE, LRESULT, PSTR},
     UI::WindowsAndMessaging::{
         self, HWND, LPARAM, WM_LBUTTONDOWN, WM_RBUTTONDOWN, WNDCLASSA, WPARAM,
     },
@@ -25,15 +27,37 @@ extern "system" fn window_proc(hwnd: HWND, msg: u32, wparam: WPARAM, lparam: LPA
 pub struct WinTray {}
 
 impl WinTray {
-    fn register_class() -> u16 {
+    fn register_class(class_name: &str, instance: HINSTANCE) -> u16 {
         let wc = WNDCLASSA {
-            lpszClassName: PSTR(b"window\0".as_ptr() as _),
+            lpszClassName: PSTR(class_name.as_bytes().as_ptr() as _),
             lpfnWndProc: Some(window_proc),
+            hInstance: instance,
             ..Default::default()
         };
         let result = unsafe { WindowsAndMessaging::RegisterClassA(&wc) };
 
         result
+    }
+
+    fn create_window(class_name: &str, instance: HINSTANCE) -> HWND {
+        let handle = unsafe {
+            WindowsAndMessaging::CreateWindowExA(
+                Default::default(),
+                class_name,
+                PSTR::default(),
+                Default::default(),
+                0,
+                0,
+                0,
+                0,
+                None,
+                None,
+                instance,
+                ptr::null_mut(),
+            )
+        };
+
+        handle
     }
 }
 
@@ -43,7 +67,18 @@ mod tests {
 
     #[test]
     fn test_register_class() {
-        let result = WinTray::register_class();
+        let class_name = "Register";
+        let instance = unsafe { SystemServices::GetModuleHandleA(None) };
+        let result = WinTray::register_class(class_name, instance);
         assert_ne!(result, 0);
+    }
+
+    #[test]
+    fn test_create_window() {
+        let class_name = "Window";
+        let instance = unsafe { SystemServices::GetModuleHandleA(None) };
+        WinTray::register_class(class_name, instance);
+        let handle = WinTray::create_window(class_name, instance);
+        assert_ne!(handle.0, 0);
     }
 }
